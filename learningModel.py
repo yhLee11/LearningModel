5b6a8dc157f0116ddf7c6a35dd309dffe35e64d2
yeonheelee <mrncd1@gmail.com>
2021-11-01 22:44:03 +0900

    edit augmentation and add learningModel module

--- learningModel.py
+++ learningModel.py
@@ -8,12 +8,13 @@ TRAIN_FOLDER = DATASET_FOLDER+'/train'
 VALID_FOLDER = DATASET_FOLDER+'/val'
 
 try:
+    lm.copy_eng_labels_to_darknet(THIS_FOLDER+'/label/eng.txt')#label폴더 안에 eng 건물 리스트를 카피
     lm.write_obj_names(DATASET_FOLDER+'/_darknet.labels')
     lm.write_obj_file(DATASET_FOLDER+'/_darknet.labels')#다크넷 파일안에 빌딩개수 읽어서 obj파일 만듬
     lm.split_train_valid()#트레인 밸리드 셋 분할
     lm.write_train_path()#트레인 경로 작성
     lm.write_valid_path()#밸리드 경로 작성
-    lm.write_config_file()#config 모듈 같은 섹션은 덮어씌워짐 해결책 찾기
+    lm.write_config_file()#config
 except:
     print('[ERROR]learning_model.py')
     exit(0)
